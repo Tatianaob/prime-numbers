bdce9d655e1c7f46b7c4d1af796c61c95e468080
Tatianaob <ltatiobandore@gmail.com>
2025-01-27 18:45:32 -0800

    Add unit test for different cases

--- test_primes.py
+++ test_primes.py
@@ -3,6 +3,7 @@ import unittest
 from .primes import get_primes_up_to_250
 
 class TestGetPrimes(unittest.TestCase):
+    # test if all primes up to 250 are in the list
     def test_prime_lists(self):
         expected_primes =  [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 
             61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 
@@ -10,5 +11,28 @@ class TestGetPrimes(unittest.TestCase):
             199, 211, 223, 227, 229, 233, 239, 241
         ]
         self.assertEqual(get_primes_up_to_250(), expected_primes)
+    
+    #Tests if certain primes are in the list
+    def test_prime_membership(self):
+        primes = get_primes_up_to_250()
+        self.assertIn(2, primes)
+        self.assertIn(3, primes)
+        self.assertIn(7, primes)
+        self.assertIn(241, primes)
 
-        
+    #test if no prime numbers are not in the list
+    def test_non_primes(self):
+        primes = get_primes_up_to_250()
+        self.assertNotIn(4, primes)
+        self.assertNotIn(9, primes)
+        self.assertNotIn(10, primes)
+        self.assertNotIn(240, primes)
+
+    # test a total of 54 prime nums up from 2 -250
+    def test_length_of_list(self):
+        primes = get_primes_up_to_250()
+        self.assertEqual(len(primes), 54)
+    
+
+if __name__ == "__main__":
+    unittest.main()
